dcbe2e6e77741d3ea7d7f49f13593b1454f26746
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-19 16:12:30 +0600

    add presentatin control

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,16 @@
-import { OrbitControls } from '@react-three/drei'
+import { Float, Environment, useGLTF, OrbitControls, PresentationControls } from '@react-three/drei'
 
 export default function Experience() {
+    const computer = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf")
     return <>
-        <color args={["#000000"]} attach={"background"} />
-        <OrbitControls makeDefault />
-
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+        <Environment preset='city' />
+        <color args={["#241a1a"]} attach={"background"} />
+        {/* <OrbitControls makeDefault /> */}
+        <PresentationControls
+            global
+        >
+            <Float rotationIntensity={0.4}>
+                <primitive object={computer.scene} position-y={-1} />
+            </Float></PresentationControls>
     </>
 }
